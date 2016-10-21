4ba7679330d1cf512f980b093365bcb4effbb2ef
sterben13 <ivan.dev.job@gmail.com>
2016-10-21 10:12:23 -0500

    video38

--- backend/views/layouts/loginLayout.php
+++ backend/views/layouts/loginLayout.php
@@ -1,16 +1,13 @@
 <?php
 
-/* @var $this \yii\web\View */
-/* @var $content string */
-
-use backend\assets\AppAsset;
+use backend\assets\LoginAsset;
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
 
-AppAsset::register($this);
+LoginAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
